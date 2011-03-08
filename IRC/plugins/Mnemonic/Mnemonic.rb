37bbe2989801a5f950f7407d3b28252da8d3eb50
Johan Sageryd <j416@1616.se>
2011-03-08 20:14:42 +0100

    Add mnemonic plugin

---
+++ IRC/plugins/Mnemonic/Mnemonic.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+# This file is part of the K5 bot project.
+# See files README.md and COPYING for copyright and licensing information.
+
+# Mnemonic plugin
+
+require 'yaml'
+require_relative '../../IRCPlugin'
+
+class Mnemonic < IRCPlugin
+	Description = "Returns mnemonics for characters."
+	Commands = { :m => "returns an mnemonic for the specified character" }
+
+	def on_privmsg(msg)
+		m = nil
+		case msg.botcommand
+		when :m
+			msg.reply m if (m = mnemonic msg.tail.split('').first)
+		end
+	end
+
+	def mnemonic(character)
+		m = YAML.load_file("#@plugin_root/mnemonics") rescue nil
+		"#{character} - #{m[character]}" if m && m[character]
+	end
+end
