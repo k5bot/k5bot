3ad5d33c17aacc7a8712c2f4f8f131cdc74ddf48
Johan Sageryd <j416@1616.se>
2011-03-08 03:05:44 +0100

    Fix bug where downcase was called on nil

--- IRC/IRCMessage.rb
+++ IRC/IRCMessage.rb
@@ -62,8 +62,8 @@ class IRCMessage
 	# The first word of the message if it starts with !
 	def botcommand
 		return unless @command == :privmsg
-		bc = message[/^\s*(#{@bot.config[:nickname]}\s*[:>,]?\s*)?!([\S]+)/i, 2].downcase if message
-		bc.to_sym if bc
+		bc = message[/^\s*(#{@bot.config[:nickname]}\s*[:>,]?\s*)?!([\S]+)/i, 2] if message
+		bc.downcase.to_sym if bc
 	end
 
 	# The last parameter
