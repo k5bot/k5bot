b260ead6007af7cf403988e6d6946528e99e6954
Johan Sageryd <j416@1616.se>
2011-03-20 16:19:13 +0100

    Add Converter plugin

---
+++ IRC/plugins/Converter/Converter.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+# This file is part of the K5 bot project.
+# See files README.md and COPYING for copyright and licensing information.
+
+# Converter plugin
+
+require_relative '../../IRCPlugin'
+
+class Converter < IRCPlugin
+	Description = "Converts units."
+	Commands = {
+		:celsius => "converts to celsius from fahrenheit",
+		:fahrenheit => "converts to fahrenheit from celsius",
+	}
+
+	def on_privmsg(msg)
+		return unless msg.tail
+		case msg.botcommand
+		when :celsius
+			msg.reply celsius(msg.tail)
+		when :fahrenheit
+			msg.reply fahrenheit(msg.tail)
+		end
+	end
+
+	def celsius(f)
+		((f.to_f - 32) * 5/9).round(2).to_s
+	end
+
+	def fahrenheit(c)
+		(c.to_f * 9/5 + 32).round(2).to_s
+	end
+end
