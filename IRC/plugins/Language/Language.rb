91403459ecf4358e1dc481d6bb0b9483890fb8e4
Johan Sageryd <j416@1616.se>
2011-03-20 21:36:41 +0100

    Add katakana->hiragana converter

--- IRC/plugins/Language/Language.rb
+++ IRC/plugins/Language/Language.rb
@@ -16,6 +16,8 @@ class Language < IRCPlugin
 	def afterLoad
 		@rom2kana = YAML.load_file("#{plugin_root}/rom2kana.yaml") rescue nil
 		@rom = @rom2kana.keys.sort_by{|x| -x.length}
+		@kata2hira = YAML.load_file("#{plugin_root}/kata2hira.yaml") rescue nil
+		@katakana = @kata2hira.keys.sort_by{|x| -x.length}
 	end
 
 	def on_privmsg(msg)
@@ -32,6 +34,13 @@ class Language < IRCPlugin
 		kana
 	end
 
+	def hiragana(katakana)
+		return katakana unless containsKatakana?(katakana)
+		hiragana = katakana.dup
+		@katakana.each{|k| hiragana.gsub!(k, @kata2hira[k])}
+		hiragana
+	end
+
 	def containsJapanese?(text)
 		# 3040-309F hiragana
 		# 30A0-30FF katakana
@@ -42,5 +51,14 @@ class Language < IRCPlugin
 		#
 		# Source: http://www.unicode.org/charts/
 		!!(text =~ /[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FC2\uFF61-\uFF9D\u31F0-\u31FF\u3000-\u303F]/)
+	end
+
+	def containsKatakana?(text)
+		# 30A0-30FF katakana
+		# FF61-FF9D half-width katakana
+		# 31F0-31FF katakana phonetic extensions
+		#
+		# Source: http://www.unicode.org/charts/
+		!!(text =~ /[\u30A0-\u30FF\uFF61-\uFF9D\u31F0-\u31FF]/)
 	end
 end
