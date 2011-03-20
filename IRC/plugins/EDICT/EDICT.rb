b75a50ba4771b572aa70570937b47e33a7992d26
Johan Sageryd <j416@1616.se>
2011-03-20 21:03:10 +0100

    Add keyword (English) lookup to EDICT plugin

--- IRC/plugins/EDICT/EDICT.rb
+++ IRC/plugins/EDICT/EDICT.rb
@@ -36,8 +36,19 @@ class EDICTEntry
 		(!@reading || @reading.empty?) ? japanese : reading
 	end
 
+	# Returns an array of the english translations and meta information.
 	def english
-		# Not yet implemented
+		@english ||= @raw.split('/')[1..-1].map{|e| e.strip}
+	end
+
+	# Returns a list of english keywords, extracted from the translations.
+	# Each keyword is a symbol.
+	def keywords
+		@keywords ||= english.map{|e| e.downcase.gsub(/[^a-z0-9 ]/, ' ').split}.flatten.map{|e| e.strip.to_sym}.sort.uniq
+	end
+
+	def common?
+		keywords.include? :p
 	end
 
 	def info
@@ -75,21 +86,27 @@ class EDICT < IRCPlugin
 		case msg.botcommand
 		when :j
 			return unless msg.tail
-			msg.reply(lookup(@l.kana(msg.tail), [:japanese, :readings]) || notFoundMsg(msg.tail))
+			entry = lookup(@l.kana(msg.tail), [:japanese, :readings])
+			msg.reply(entry ? entry.to_s : notFoundMsg(msg.tail))
 		when :e
 			return unless msg.tail
-			msg.reply(lookup(msg.tail, [:english]) || notFoundMsg(msg.tail))
+			entry = keywordLookup(msg.tail)
+			msg.reply(entry ? entry.to_s : notFoundMsg(msg.tail))
 		when :next
 			count = msg.tail.to_i
 			count = (count > 0) ? count : 1
 			count = 5 if count > 5
 			count.times do
 				if nextReply = lookupNext
-					msg.reply(nextReply)
+					msg.reply nextReply.to_s
 				else
-					msg.reply(notFoundMsg)
+					msg.reply notFoundMsg
 					break
 				end
+			end
+		when :keywords
+			if entry = lookup(@l.kana(msg.tail), [:japanese, :readings])
+				msg.reply entry.keywords*', '
 			end
 		end
 	end
@@ -105,10 +122,34 @@ class EDICT < IRCPlugin
 		lookupNext
 	end
 
+	# Looks up keywords in the keyword hash.
+	# Specified argument is a string of one or more keywords.
+	# Returns the intersection of the results for each keyword.
+	def keywordLookup(word)
+		@lastWord = word
+		@lookupResult = nil
+		keywords = word.downcase.gsub(/[^a-z0-9 ]/, '').split(' ').uniq
+		keywords.each do |k|
+			unless (entryArray = @hash[:keywords][k.to_sym])
+				@lookupResult = nil
+				return nil
+			end
+			if @lookupResult
+				@lookupResult &= entryArray
+			else
+				@lookupResult = Array.new(entryArray)
+			end
+		end
+		if @lookupResult
+			@lookupResult.sort_by!{|e| [(e.common? ? -1 : 1), e.keywords.size]}
+		end
+		lookupNext
+	end
+
 	def lookupNext
 		return unless @lookupResult
 		if entry = @lookupResult.shift
-			entry.to_s
+			entry
 		end
 	end
 
@@ -122,7 +163,7 @@ class EDICT < IRCPlugin
 		@hash = {}
 		@hash[:japanese] = {}
 		@hash[:readings] = {}
-		@hash[:english] = {}
+		@hash[:keywords] = {}
 		@lastWord = nil
 		@lookupResult = nil
 		edictfile = "#{(File.dirname __FILE__)}/edict"
@@ -131,6 +172,9 @@ class EDICT < IRCPlugin
 				entry = EDICTEntry.new(Iconv.conv('UTF-8', 'EUC-JP', l).strip)
 				(@hash[:japanese][entry.japanese] ||= []) << entry
 				(@hash[:readings][entry.reading] ||= []) << entry
+				entry.keywords.each do |k|
+					(@hash[:keywords][k] ||= []) << entry
+				end
 			end
 		end
 	end
