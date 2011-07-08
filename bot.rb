989991c8973d6f64f0af975af4b816ca795b5c87
Johan Sageryd <j416@1616.se>
2011-07-09 00:16:57 +0200

    Fix compatibility

--- bot.rb
+++ bot.rb
@@ -1,7 +1,9 @@
-#!/usr/bin/env ruby -w
+#!/usr/bin/env ruby
 # encoding: utf-8
 # This file is part of the K5 bot project.
 # See files README.md and COPYING for copyright and licensing information.
+
+$VERBOSE = true
 
 require 'yaml'
 require_relative 'IRC/IRCBot'
