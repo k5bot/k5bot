a685f61360cb8319542e3a0f8b761fb604ed6df0
Johan Sageryd <j416@1616.se>
2012-01-07 22:27:03 +0100

    Add Dice plugin

---
+++ IRC/plugins/Dice/Dice.rb
@@ -0,0 +1,43 @@
+# encoding: utf-8
+# This file is part of the K5 bot project.
+# See files README.md and COPYING for copyright and licensing information.
+
+# Dice plugin
+
+require_relative '../../IRCPlugin'
+
+class Dice < IRCPlugin
+  Description = "Dice plugin."
+  Commands = {
+    :roll => "rolls the dice",
+  }
+
+  def on_privmsg(msg)
+    case msg.botcommand
+    when :roll
+      die1 = roll
+      die2 = roll
+      rollResult = '%s and %s, total is %s' % [ numberToText(die1), numberToText(die2), numberToText(die1 + die2) ]
+      msg.reply(rollResult)
+    end
+  end
+
+  def roll
+    rand(6) + 1
+  end
+
+  def numberToText(num)
+    [ 'one',
+      'two',
+      'three',
+      'four',
+      'five',
+      'six',
+      'seven',
+      'eight',
+      'nine',
+      'ten',
+      'eleven',
+      'twelve' ][num - 1]
+  end
+end
