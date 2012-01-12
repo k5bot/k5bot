7b79b200a956841ab347d02cbc84864b6db20fb3
Johan Sageryd <j416@1616.se>
2012-01-12 23:19:41 +0100

    Add version command

--- IRC/plugins/Statistics/Statistics.rb
+++ IRC/plugins/Statistics/Statistics.rb
@@ -9,13 +9,16 @@ require_relative '../../IRCPlugin'
 class Statistics < IRCPlugin
   Description = "A plugin that keeps track of various statistics."
   Commands = {
-    :uptime => "shows how long the bot has been running"
+    :uptime => "shows how long the bot has been running",
+    :version => "shows the current bot version"
   }
 
   def on_privmsg(msg)
     case msg.botcommand
     when :uptime
       msg.reply(uptimeString)
+    when :version
+      msg.reply(versionString)
     end
   end
 
@@ -27,6 +30,10 @@ class Statistics < IRCPlugin
     u[:day], u[:hour] = u[:hour].divmod(24)
     u[:week], u[:day] = u[:day].divmod(7)
     'up ' + [:week, :day, :hour, :minute, :second].map { |unit| u[unit].floor == 0 ? nil : "%d %s" % [u[unit], pluralize(unit.to_s, u[unit])] }.reject { |unit| unit.nil? }.join(', ')
+  end
+
+  def versionString
+    `GIT_DIR=#{File.dirname($0)}/.git $(which git) rev-parse --short HEAD`;
   end
 
   def pluralize(str, num)
