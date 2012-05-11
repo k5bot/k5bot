d66b14c5c73acafc18d69b754a12807b1dd88605
Johan Sageryd <j416@1616.se>
2012-05-11 21:13:45 +0200

    Add Git plugin

---
+++ IRC/plugins/Git/Git.rb
@@ -0,0 +1,41 @@
+# encoding: utf-8
+# This file is part of the K5 bot project.
+# See files README.md and COPYING for copyright and licensing information.
+
+# Git plugin
+
+require_relative '../../IRCPlugin'
+
+class Git < IRCPlugin
+  Description = "Plugin to run git pull."
+  Commands = {
+    :pull => "runs git pull"
+  }
+  Dependencies = [ :Statistics ]
+
+  def afterLoad
+    @l = @bot.pluginManager.plugins[:Statistics]
+  end
+
+  def beforeUnload
+    @l = nil
+  end
+
+  def on_privmsg(msg)
+    case msg.botcommand
+    when :pull
+      versionBefore = @l.versionString
+      gitPull
+      versionAfter = @l.versionString
+      if versionBefore == versionAfter
+        msg.reply('Already up-to-date.')
+      else
+        msg.reply("Updating #{versionBefore} -> #{versionAfter}")
+      end
+    end
+  end
+
+  def gitPull
+    `pushd #{File.dirname($0)} && $(which git) pull && popd`
+  end
+end
