801c2d08b84dd793b07a7915be468fb849cacebe
Johan Sageryd <j416@1616.se>
2012-08-05 12:01:11 +0200

    Add Dotdotdot plugin

---
+++ IRC/plugins/Dotdotdot/Dotdotdot.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+# This file is part of the K5 bot project.
+# See files README.md and COPYING for copyright and licensing information.
+
+# Dotdotdot plugin
+
+require_relative '../../IRCPlugin'
+
+class Dotdotdot < IRCPlugin
+  Description = "..."
+
+  def on_privmsg(msg)
+    (c = msg.message.count('.')) < 20 && msg.reply('.' * (c + 1)) || msg.reply('...') if msg.message =~ /^\s*(\.\s*)+\s*$/
+  end
+end
