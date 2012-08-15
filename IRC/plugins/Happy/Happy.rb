3886852d90299adf876a0b80d8cf6905af56d5e9
Johan Sageryd <j416@1616.se>
2012-08-15 21:00:49 +0200

    Add Happy plugin

---
+++ IRC/plugins/Happy/Happy.rb
@@ -0,0 +1,51 @@
+# encoding: utf-8
+# This file is part of the K5 bot project.
+# See files README.md and COPYING for copyright and licensing information.
+
+# Happy plugin
+
+require_relative '../../IRCPlugin'
+
+class Happy < IRCPlugin
+  Description = ":D"
+
+  Happy = [
+    ':)',
+    ':D',
+    'D:',
+    '^^',
+    ':|',
+    ':/',
+    ':O',
+    ':#',
+    ':v',
+    '\(^o^)/',
+    'o_O',
+    'O_O',
+    'O_o',
+    'O_( )',
+    '囧',
+    '冏',
+    ':C',
+    '(:',
+    ':x',
+    'XD',
+    '8|',
+    ':@',
+    '-_-',
+    ':(',
+    '):',
+    ';)',
+    '=)',
+    '=D',
+    ':P',
+    '::)',
+    ':))',
+    'o/',
+    '\o'
+  ]
+
+  def on_privmsg(msg)
+    msg.reply(self.class::Happy.sample) if msg.message =~ /^\s*(#{@bot.user.nick}\s*[:>,]?\s*)?(#{self.class::Happy.map { |s| Regexp.quote(s) }.join('|')})+\s*$/
+  end
+end
