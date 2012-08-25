c1edd995ba9b1db58fc3bb9a973725125c575dc9
Alexander Belykh <albel727@ngs.ru>
2012-08-25 13:07:43 +0200

    Add Manage plugin

---
+++ IRC/plugins/Manage/Manage.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+# This file is part of the K5 bot project.
+# See files README.md and COPYING for copyright and licensing information.
+
+# Manage plugin
+
+require_relative '../../IRCPlugin'
+
+class Manage < IRCPlugin
+  Description = "A plugin for basic bot management."
+  Commands = {
+    :join => "joins specified channel(s)",
+    :part => "parts from specified channel(s)"
+  }
+
+  def on_privmsg(msg)
+    case msg.botcommand
+    when :join
+      @bot.send "JOIN #{msg.tail.split(/[;,\s]+/).join(',')}"
+    when :part
+      @bot.send "PART #{msg.tail.split(/[;,\s]+/).join(',')}"
+    end
+  end
+end
