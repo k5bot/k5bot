cf3e6ef5f1ad38d7b091f5a1d83a072cac6a6d6a
Johan Sageryd <j416@1616.se>
2012-09-09 23:15:40 +0200

    Add .upstream command to Git plugin

--- IRC/plugins/Git/Git.rb
+++ IRC/plugins/Git/Git.rb
@@ -9,7 +9,8 @@ require_relative '../../IRCPlugin'
 class Git < IRCPlugin
   Description = "Plugin to run git pull."
   Commands = {
-    :pull => "fetches changes from upstream and resets current branch and working directory to reflect it"
+    :pull => "fetches changes from upstream and resets current branch and working directory to reflect it",
+    :upstream => "shows from where changes are pulled"
   }
   Dependencies = [ :Statistics ]
 
@@ -32,10 +33,20 @@ class Git < IRCPlugin
       else
         msg.reply("Updating #{versionBefore} -> #{versionAfter}")
       end
+    when :upstream
+      msg.reply(gitUpstream)
     end
   end
 
   def gitPull
     `pushd #{File.dirname($0)} && $(which git) fetch && $(which git) reset --hard @{upstream} && popd`
+  end
+
+  def gitUpstream
+    upstream = `pushd #{File.dirname($0)} >/dev/null 2>&1 && $(which git) branch -vv | grep -e '^\*' && popd >/dev/null 2>&1`[/\[([^\[]+)\]/, 1]
+    remote = upstream[/([^\/]+)\/([^:]+).*/, 1]
+    branch = upstream[/([^\/]+)\/([^:]+).*/, 2]
+    remoteURL = `pushd #{File.dirname($0)} >/dev/null 2>&1 && $(which git) remote -v | grep #{remote} | head -1 && popd >/dev/null 2>&1`.split[1]
+    "upstream is [#{branch}] at #{remoteURL}"
   end
 end
