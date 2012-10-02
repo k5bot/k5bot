9f4cdaf02bcdf653547210c8270910a29203ed1c
Alexander Belykh <albel727@ngs.ru>
2012-10-02 14:20:00 +0700

    Fix helper class reloading

--- IRC/IRCPlugin.rb
+++ IRC/IRCPlugin.rb
@@ -48,8 +48,9 @@ class IRCPlugin < IRCListener
 
   def load_helper_class(class_name)
     class_name = class_name.to_sym
+
+    unload_helper_class(class_name)
     begin
-      Object.send :remove_const, class_name
       load "#{plugin_root}/#{class_name}.rb"
     rescue ScriptError, StandardError => e
       puts "Cannot load #{class_name}: #{e}"
