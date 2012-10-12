a6a213749f6936fb5ac11d6119c0f425fc2112a4
Alexander Belykh <albel727@ngs.ru>
2012-10-12 13:28:34 +0700

    Add .utime and .jtime to Clock

--- IRC/plugins/Clock/Clock.rb
+++ IRC/plugins/Clock/Clock.rb
@@ -8,16 +8,31 @@ require_relative '../../IRCPlugin'
 
 class Clock < IRCPlugin
   Description = "The Clock plugin tells the time."
-  Commands = { :time => 'tells the current time' }
+  Commands = {
+      :time => 'tells the current time',
+      :jtime => 'tells the current time in Japan only',
+      :utime => 'tells the current time in UTC only'
+  }
 
   def on_privmsg(msg)
     case msg.botcommand
     when :time
-      msg.reply time
+      time = Time.now
+      msg.reply "#{jtime(time)} | #{utime(time)}"
+    when :jtime
+      time = Time.now
+      msg.reply jtime(time)
+    when :utime
+      time = Time.now
+      msg.reply utime(time)
     end
   end
 
-  def time
-    Time.now.utc
+  def utime(t)
+    Time.at(t).utc
+  end
+
+  def jtime(t)
+    Time.at(t).localtime("+09:00").strftime '%Y-%m-%d %H:%M:%S JST'
   end
 end
