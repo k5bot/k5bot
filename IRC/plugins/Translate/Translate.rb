1b56136f24fd13c9b56231043b01b127ca8f4b0f
Alexander Belykh <albel727@ngs.ru>
2012-10-13 09:19:18 +0700

    Replace OCN with Honyaku translation service

--- IRC/plugins/Translate/Translate.rb
+++ IRC/plugins/Translate/Translate.rb
@@ -8,9 +8,10 @@ require_relative '../../IRCPlugin'
 require 'rubygems'
 require 'nokogiri'
 require 'net/http'
+require 'json'
 
 class Translate < IRCPlugin
-  Description = "Uses the translation engine from www.ocn.ne.jp to translate between languages."
+  Description = "Uses translation engines to translate between languages."
   Commands = {
     :t  => "determines if specified text is Japanese or not, then translates appropriately J>E or E>J",
     :gt => "same as .t, but uses Google Translate",
@@ -58,21 +59,6 @@ class Translate < IRCPlugin
     end
   end
 
-  def ocnTranslate(text, lp)
-    prm = '63676930312e6f6' + '36e2e6e652e6a70'
-    result = Net::HTTP.get(URI.parse("http://cgi01.ocn.ne.jp/cgi-bin/translation/counter.cgi?prm=#{prm}"))
-    key = result[/value='([^']+)'/, 1]
-    result = Net::HTTP.post_form(
-      URI.parse('http://cgi01.ocn.ne.jp/cgi-bin/translation/index.cgi'),
-      {'sourceText' => text, 'langpair' => lp, 'auth' => key})
-    result.body.force_encoding 'utf-8'
-    return if [Net::HTTPSuccess, Net::HTTPRedirection].include? result
-    doc = Nokogiri::HTML result.body
-    doc.css('textarea[name = "responseText"]').text.chomp
-  rescue => e
-    puts "Cannot translate: #{e}\n\t#{e.backtrace.join("\n\t")}"
-  end
-
   def googleTranslate(text, lp)
     result = Net::HTTP.post_form(
       URI.parse('http://translate.google.com'),
@@ -82,6 +68,78 @@ class Translate < IRCPlugin
     doc.css('span[id="result_box"] span').text.chomp
   end
 
-  alias translate ocnTranslate
+  HONYAKU_INIT_URL="http://honyaku.yahoo.co.jp/transtext/"
+  HONYAKU_BASE_URL="http://honyaku.yahoo.co.jp/TranslationText"
+  HONYAKU_USER_AGENT="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET"
+
+  def honyaku_get_json(str, type)
+    http_obj = Net::HTTP
+    ieid = type[0..1]
+    oeid = type[2..3]
+
+    # Obtain crumb # TODO: rewrite with nokogiri
+    crumb = nil
+    uri = URI.parse(HONYAKU_INIT_URL)
+    http_obj.start(uri.host, uri.port) do |http|
+      request = Net::HTTP::Get.new(uri.path)
+      request["user-agent"] = HONYAKU_USER_AGENT
+      res = http.request(request)
+      if res.body == nil || res.body.empty?
+        next
+      end
+      # <input type="hidden" id="TTcrumb" name="TTcrumb" value="..."/>
+      res.body.each_line do |line|
+        next if line.index('id="TTcrumb"') == nil
+        _ , crumb1 = line.chomp.split('value="')
+        crumb, _ = crumb1.split('"')
+        break
+      end
+    end
+
+    # Call translation API
+    uri = URI.parse(HONYAKU_BASE_URL)
+    http_obj.start(uri.host, uri.port) do |http|
+      request = Net::HTTP::Post.new(uri.path)
+      request["user-agent"]       = HONYAKU_USER_AGENT
+      request["referer"]          = HONYAKU_INIT_URL
+      request["x-requested-with"] = "XMLHttpRequest"
+      request["Accept-Language"]  = "ja"
+      request["Accept"]           = "application/json, text/javascript, */*"
+      body = {
+          :ieid      => ieid,
+          :oeid      => oeid,
+          :results   => 1000,
+          :formality => 0,
+          :output    => "json",
+          :p         => str,
+          :_crumb    => crumb,
+      }
+      request.set_form_data(body)
+
+      res = http.request(request)
+      return res
+    end
+  end
+
+  def honyakuTranslate(str, type)
+    ret = honyaku_get_json(str, type)
+
+    json = JSON.parse(ret.body)
+
+    if  json != nil and
+        json.include?("ResultSet") and
+        json["ResultSet"].include?("ResultText") and
+        json["ResultSet"]["ResultText"].include?("Results")
+
+      results = json["ResultSet"]["ResultText"]["Results"]
+
+      results.map { |result| result["TranslatedText"] }.join(' ')
+    else
+      puts "failed... received data: #{ret.body}"
+      nil
+    end
+  end
+
+  alias translate honyakuTranslate
   alias gtranslate googleTranslate
 end
