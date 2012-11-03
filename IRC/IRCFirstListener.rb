ee5e2ca7fe167bb6400b65a2aaf53287772f8fa5
Alexander Belykh <albel727@ngs.ru>
2012-11-04 01:44:15 +0700

    Prevent message propagation past IRCFirstListener

--- IRC/IRCFirstListener.rb
+++ IRC/IRCFirstListener.rb
@@ -8,21 +8,37 @@
 require_relative 'IRCListener'
 
 class IRCFirstListener < IRCListener
+
+  # This method is overridden, so that command-methods can
+  # pass back their own return values.
+  def receive_message(msg)
+    dispatch_message_to_methods(msg)
+  end
+
   def on_ping(msg)
     msg.bot.send_raw(msg.params ? "PONG :#{msg.params.first}" : 'PONG')
+
+    true # stop further message propagation
   end
 
   def on_263
     msg.bot.send_raw(msg.bot.last_sent)
+
+    true # stop further message propagation
   end
 
   def on_privmsg(msg)
+    result = nil
+
     queries = msg.ctcp
     queries.each do |ctcp|
       case ctcp[:command]
         when :PING
           msg.notice_user(IRCMessage.make_ctcp_message(:PING, ctcp[:arguments]))
+          result = true # stop further message propagation
       end
     end
+
+    result # stop further message propagation, if it was CTCP query that we handled
   end
 end
