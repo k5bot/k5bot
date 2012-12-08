cd7186d824cd30e9e5994c672e01d4df4a82c9b6
Alexander Belykh <albel727@ngs.ru>
2012-12-09 00:32:45 +0700

    Hack Loader for brute-force reload in correct order

--- IRC/plugins/Loader/Loader.rb
+++ IRC/plugins/Loader/Loader.rb
@@ -18,18 +18,40 @@ class Loader < IRCPlugin
     case msg.botcommand
     when :load
       return unless msg.tail
-      msg.tail.split.each do |name|
+      plugins = msg.tail.split
+
+      plugins_to_load = {}
+
+      cycle_while_reducing(plugins) do |name|
         exists = !!(@plugin_manager.plugins[name.to_sym])
-        unloadSuccessful = !!(@plugin_manager.unload_plugin name)
-        if unloadSuccessful || !exists
-          if @plugin_manager.load_plugin(name)
-            msg.reply "'#{name}' #{'re' if exists}loaded."
-          else
-            msg.reply "Cannot #{'re' if exists}load '#{name}'."
-          end
+        unload_successful = !exists || !!(@plugin_manager.unload_plugin name)
+        if unload_successful
+          plugins_to_load[name] = exists
+          true
         else
-          msg.reply "Cannot reload '#{name}'."
+          false
         end
+      end
+
+      plugins.each do |name|
+        msg.reply "Cannot unload '#{name}'."
+      end
+
+      # Since hash in Ruby 1.9 preserves order of addition,
+      # reverse order would be the best one for loading
+      plugins_to_load = plugins_to_load.to_a.reverse!
+
+      cycle_while_reducing(plugins_to_load) do |name, existed|
+        if @plugin_manager.load_plugin(name)
+          msg.reply "'#{name}' #{'re' if existed}loaded."
+          true
+        else
+          false
+        end
+      end
+
+      plugins_to_load.each do |name, existed|
+        msg.reply "Cannot #{'re' if existed}load '#{name}'."
       end
     when :unload
       return unless msg.tail
@@ -55,4 +77,13 @@ class Loader < IRCPlugin
       msg.reply "Core files reloaded."
     end
   end
+
+  def cycle_while_reducing(collection)
+    old_size = collection.size + 1
+    until collection.empty? || old_size <= collection.size
+      old_size = collection.size
+      collection.delete_if {|*v| yield(*v)}
+    end
+  end
+
 end
