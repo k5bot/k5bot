9cb9e7f80001c59b75bb195320028dfb607f27e3
Alexander Belykh <albel727@ngs.ru>
2013-01-10 07:29:39 +0700

    Improve timezone support and formatting in Clock
    Added support for ISO-3166 country codes,
    case-sensitive timezone abbreviations,
    and full timezone names.

--- IRC/plugins/Clock/Clock.rb
+++ IRC/plugins/Clock/Clock.rb
@@ -12,11 +12,17 @@ require_relative '../../IRCPlugin'
 class Clock < IRCPlugin
   Description = "The Clock plugin tells the time."
   Commands = {
-      :time => 'tells the current time. Optionally accepts space-separated list of timezones',
+      :time => "tells the current time. \
+Optionally accepts space-separated list of timezone identifiers (e.g. Asia/Tokyo), \
+timezone abbreviations (e.g. UTC, JST), \
+and ISO-3166 country names (e.g. US, JP)",
       :jtime => 'tells the current time in Japan only',
       :utime => 'tells the current time in UTC only'
   }
 
+  def normalize_zone_identifier(id)
+    id.upcase.gsub(/_/,'').gsub(/-(\D)/,'\1')
+  end
 
   def afterLoad
     # Try to gather all known zone abbreviations
@@ -41,6 +47,17 @@ class Clock < IRCPlugin
         @zone_by_abbreviation[abbrev] |= [zone]
       end
     end
+
+    # Timezone.get() doesn't accept arbitrary case,
+    # so let's compose our own case-insensitive table.
+    @zone_by_identifier = {}
+
+    TZInfo::Timezone.all.each do |zone|
+      identifier = normalize_zone_identifier(zone.identifier)
+      @zone_by_identifier.merge!(identifier => zone) do |key, old_val, new_val|
+        raise "Can't disambiguate timezone #{key}: it's either #{old_val} or #{new_val}"
+      end
+    end
   end
 
   def beforeUnload
@@ -62,12 +79,28 @@ class Clock < IRCPlugin
     end
   end
 
-  def time(time, abbrevs)
+  def time(time, search_terms)
     time = Time.at(time).utc # convert time to UTC, or strftime won't work properly
 
-    abbrevs.map do |abbrev|
-      abbrev.upcase!
-      zones = @zone_by_abbreviation[abbrev.to_sym]
+    search_terms.map do |search_term|
+      presorted = false
+
+      # Try to search by abbreviation verbatim first.
+      # Useful to differentiate e.g. 'MEST' vs 'MeST'.
+      zones = @zone_by_abbreviation[search_term.to_sym]
+      # If that didn't help, try upcase abbreviation.
+      zones ||= @zone_by_abbreviation[search_term.upcase.to_sym]
+      # Maybe it's a ISO 3166 country code, e.g. 'US'?
+      unless zones
+        zones = TZInfo::Country.get(search_term.upcase).zones rescue nil
+        # Country.get() returns array ordered by relevancy descending.
+        presorted = true
+      end
+      # Finally, maybe it's a zone identifier, e.g. 'America/New_York'?
+      unless zones
+        zones = @zone_by_identifier[normalize_zone_identifier(search_term)]
+        zones = [zones] if zones
+      end
 
       if zones
         # Zone abbreviations are actually ambiguous.
@@ -75,11 +108,17 @@ class Clock < IRCPlugin
         zones.group_by do |zone|
           zone.strftime('%Y-%m-%d %H:%M:%S %Z', time)
         end.map do |format, sub_zones|
-          best_zone = sub_zones.map { |z| z.identifier }.min_by { |id| id.size }
-          "#{format} (in #{best_zone}#{',...' if sub_zones.size>1})"
-        end.sort.join(', ')
+          best_zone = if presorted
+                        # group_by() preserved desired order, the best one is the first.
+                        sub_zones[0].identifier
+                      else
+                        # otherwise, find the shortest identifier.
+                        sub_zones.map { |z| z.identifier }.min_by { |id| id.size }
+                      end
+          "#{format}(#{best_zone}#{',...' if sub_zones.size>1})"
+        end.sort.join('; ')
       else
-        "Unknown timezone #{abbrev}"
+        "Unknown timezone '#{search_term}'"
       end
     end.join(' | ')
   end
