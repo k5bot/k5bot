a324ef09264fc409abe4ec171818e84601443ee9
Alexander Belykh <albel727@ngs.ru>
2013-01-28 00:41:07 +0700

    Add from_hangul() method to Language

--- IRC/plugins/Language/Language.rb
+++ IRC/plugins/Language/Language.rb
@@ -7,6 +7,7 @@
 require 'yaml'
 require_relative '../../IRCPlugin'
 
+#noinspection RubyLiteralArrayInspection
 class Language < IRCPlugin
   attr_reader :unicode_desc
 
@@ -14,6 +15,30 @@ class Language < IRCPlugin
   Commands = {
     :kana => 'converts specified romazi to kana'
   }
+
+  JAMO_L_TABLE = [
+    'g', 'gg', 'n', 'd', 'dd', 'r', 'm', 'b', 'bb',
+    's', 'ss', '', 'j', 'jj', 'ch', 'k', 't', 'p', 'h'
+  ] # CH is C in ISO/TR 11941
+
+  JAMO_V_TABLE = [
+    'a', 'ae', 'ya', 'yae', 'eo', 'e', 'yeo', 'ye', 'o',
+    'wa', 'wae', 'oe', 'yo', 'u', 'weo', 'we', 'wi',
+    'yu', 'eu', 'eui', 'i'
+  ] # EUI is YI in ISO/TR 11941
+
+  JAMO_T_TABLE = [
+    '', 'g', 'gg', 'gs', 'n', 'nj', 'nh', 'd', 'l', 'lg', 'lm',
+    'lb', 'ls', 'lt', 'lp', 'lh', 'm', 'b', 'bs',
+    's', 'ss', 'ng', 'j', 'c', 'k', 't', 'p', 'h'
+  ]
+
+  HANGUL_S_BASE = 0xAC00
+  HANGUL_L_COUNT = 19
+  HANGUL_V_COUNT = 21
+  HANGUL_T_COUNT = 28
+  HANGUL_N_COUNT = HANGUL_V_COUNT * HANGUL_T_COUNT # 588
+  HANGUL_S_COUNT = HANGUL_L_COUNT * HANGUL_N_COUNT # 11172
 
   def afterLoad
     @rom2kana = YAML.load_file("#{plugin_root}/rom2kana.yaml") rescue nil
@@ -43,6 +68,24 @@ class Language < IRCPlugin
     hiragana = katakana.dup
     @katakana.each{|k| hiragana.gsub!(k, @kata2hira[k])}
     hiragana
+  end
+
+  # This method is a slightly modified copy of the implementation found at:
+  # <a href="http://www.unicode.org/reports/tr15/tr15-29.html#Hangul">http://www.unicode.org/reports/tr15/tr15-29.html#Hangul</a>
+  # @param [String] hangul symbols
+  # @return array of names of the characters
+  def from_hangul(hangul)
+    hangul.unpack("U*").map do |codepoint|
+      s_index = codepoint - HANGUL_S_BASE
+
+      raise "Not a Hangul syllable: #{hangul}" if (0 > s_index) || (s_index >= HANGUL_S_COUNT)
+
+      l_index = s_index / HANGUL_N_COUNT
+      v_index = (s_index % HANGUL_N_COUNT) / HANGUL_T_COUNT
+      t_index = s_index % HANGUL_T_COUNT
+
+      JAMO_L_TABLE[l_index] + JAMO_V_TABLE[v_index] + JAMO_T_TABLE[t_index]
+    end
   end
 
   def containsJapanese?(text)
