7f142d8e04a0911a7cdec2afe2c6080467345b93
Alexander Belykh <albel727@ngs.ru>
2013-04-16 19:55:14 +0700

    Add irc server capabilities querying

--- IRC/plugins/IRCBot/IRCLoginListener.rb
+++ IRC/plugins/IRCBot/IRCLoginListener.rb
@@ -5,17 +5,31 @@
 # IRCLoginListener is the listener that handles login-time interaction
 # with an IRC server, which includes auth, setting nick, etc.
 
+require 'set'
+
 require_relative '../../IRCListener'
 
 class IRCLoginListener
   include IRCListener
 
+  attr_reader :server_capabilities
+
   def initialize(bot)
     @bot = bot
+    @server_capabilities = Set.new()
   end
 
   def login
     config = @bot.config
+
+    # Reset capabilities, that could have been left over since last bot connect.
+    @server_capabilities = Set.new()
+
+    # Query for extended capabilities supported by server
+    @bot.send 'CAP LS'
+    # Make it immediately known, that we don't intend to use any,
+    # b/c authentication is suspended/impossible until that.
+    @bot.send 'CAP END'
 
     @bot.send "PASS #{config[:serverpass]}" if config[:serverpass]
     @bot.send "NICK #{config[:nickname]}" if config[:nickname]
@@ -25,6 +39,10 @@ class IRCLoginListener
     else
       @bot.post_login
     end
+  end
+
+  def on_cap(msg)
+    @server_capabilities |= msg.message.split.map {|x| x.downcase.to_sym}
   end
 
   def on_notice(msg)
