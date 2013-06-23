a9770a59afe3a01dc017a39f7de48fd4571c6e6d
Alexander Belykh <albel727@ngs.ru>
2013-06-23 22:05:11 +0700

    Fix unnecessary 1st line cut in CEDICT's download.sh

--- IRC/plugins/CEDICT/download.sh
+++ IRC/plugins/CEDICT/download.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # This file is part of the K5 bot project.
 # See files README.md and COPYING for copyright and licensing information.
-curl http://www.mdbg.net/chindict/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz | gunzip | sed "1 d" > cedict
+curl http://www.mdbg.net/chindict/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz | gunzip > cedict
