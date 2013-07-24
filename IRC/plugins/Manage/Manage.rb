2e06a6a123d6e03842886b6cea07ab5d15dc0f9c
Alexander Belykh <albel727@ngs.ru>
2013-07-25 02:06:30 +0700

    Add .demonstrate command to Manage

--- IRC/plugins/Manage/Manage.rb
+++ IRC/plugins/Manage/Manage.rb
@@ -15,6 +15,10 @@ class Manage < IRCPlugin
     :raw_ctcp => "<destination> <command> [args] - sends custom CTCP PRIVMSG \
 command to destination",
     :kill => 'kills current connection',
+    :demonstrate => "accepts raw message without prefix, sends it to server \
+and then acts as if received it in the usual way from sender. Example: \
+.demonstrate PRIVMSG ##japanese :.help <-- prints '.help' followed \
+by help contents, as if sender was in ##japanese and issued .help command"
   }
   Dependencies = [ :Router ]
 
@@ -24,7 +28,7 @@ command to destination",
     return if dispatch_message_by_command(msg, [:join, :part]) do
       check_and_complain(@plugin_manager.plugins[:Router], msg, :can_join_channels)
     end
-    dispatch_message_by_command(msg, [:raw, :kill, :raw_ctcp]) do
+    dispatch_message_by_command(msg, [:raw, :kill, :raw_ctcp, :demonstrate]) do
       check_and_complain(@plugin_manager.plugins[:Router], msg, :can_do_everything)
     end
   end
@@ -46,6 +50,11 @@ command to destination",
     destination = args.shift
     cmd = args.shift
     msg.bot.send_raw("PRIVMSG #{destination} :#{IRCMessage.make_ctcp_message(cmd, args)}")
+  end
+
+  def cmd_demonstrate(msg)
+    msg.bot.send_raw(msg.tail)
+    msg.bot.receive(':' + msg.prefix + ' ' + msg.tail + "\r\n")
   end
 
   def cmd_kill(msg)
