6fd229edf4e00e6f22cacbdc92cec7d6c444b6f9
Alexander Belykh <albel727@ngs.ru>
2013-07-25 02:21:19 +0700

    Add .seenwhile command in Seen plugin

--- IRC/plugins/Seen/Seen.rb
+++ IRC/plugins/Seen/Seen.rb
@@ -9,7 +9,10 @@ require_relative '../../IRCPlugin'
 class Seen < IRCPlugin
   Description = 'A plugin that keeps track of when a user was last seen and where.'
   Commands = {
-    :seen => '[nick] (ex.: !seen K5) gives information on when [nick] was last seen'
+    :seen => "[nick] - gives information on when [nick] was last seen. \
+Example: .seen K5",
+    :seenwhile => "[text] - specifies text to show whenever someone uses \
+.seen command on you. Example: .seenwhile drinking beer",
   }
 
   Dependencies = [ :StorageYAML, :UserPool ]
@@ -47,7 +50,12 @@ class Seen < IRCPlugin
       sought_nick = msg.tail
 
       if !sought_nick || sought_nick.casecmp(msg.nick) == 0
-        msg.reply("#{msg.nick}: watching your every move.")
+        seen_data = get_seen_info(msg.user)
+        if seen_data && seen_data[:while]
+          msg.reply("#{msg.nick}: watching you #{seen_data[:while]}.")
+        else
+          msg.reply("#{msg.nick}: watching your every move.")
+        end
         return
       end
       if sought_nick.casecmp(msg.bot.user.nick) == 0
@@ -58,17 +66,23 @@ class Seen < IRCPlugin
       sought_user = @user_pool.findUserByNick(msg.bot, sought_nick)
 
       if sought_user && sought_user.name
-        seen_data = @seen[user_to_uid(sought_user)]
+        seen_data = get_seen_info(sought_user)
         if seen_data && seen_data[:time]
           as = format_ago_string(seen_data[:time])
           if seen_data[:channel] && msg.channelname
             in_this_channel = seen_data[:channel] == msg.channelname
             cs = in_this_channel ? 'in this channel' : 'in another channel'
-            m = in_this_channel && seen_data[:message] ? ' saying: ' + truncate(seen_data[:message], 80) : '.'
+            m = in_this_channel && seen_data[:message] && ('saying: ' + truncate(seen_data[:message], 80))
           else
             cs = nil
-            m = '.'
+            m = nil
           end
+          # If we have both seen-while text and user message, connect them with 'and'
+          m_tmp = [seen_data[:while], m].reject {|x| !x}.to_a.join(' and ')
+          # Don't add excess trailing dot, if we have user message coming last.
+          # Don't add leading space, if we have nothing else to type.
+          m = "#{' ' + m_tmp unless m_tmp.empty?}#{'.' unless m}"
+
           msg.reply("#{sought_user.nick} was last seen #{as + ' ' if as}#{cs + ' ' if cs}".rstrip + m)
         else
           msg.reply("#{msg.nick}: I have not seen #{sought_user.nick}.")
@@ -76,7 +90,18 @@ class Seen < IRCPlugin
       else
         msg.reply("#{msg.nick}: I do not know who that is.")
       end
+    when :seenwhile
+      update_seen_info(msg.user, {:while => msg.tail})
+      if msg.tail
+        msg.reply("#{msg.nick}: So THAT's how you call it...")
+      else
+        msg.reply("#{msg.nick}: Ok, I'll pretend I haven't seen you doing anything...")
+      end
     end
+  end
+
+  def get_seen_info(user)
+    @seen[user_to_uid(user)]
   end
 
   def user_to_uid(user)
