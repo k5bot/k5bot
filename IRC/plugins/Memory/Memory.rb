4134099d37f4796b061f1542eb84e42d34e9d856
Thuruk <d.staudigl@gmx.at>
2013-09-05 15:28:38 +0200

    Add Memory plugin

---
+++ IRC/plugins/Memory/Memory.rb
@@ -0,0 +1,16 @@
+require_relative '../../IRCPlugin'
+
+class Memory < IRCPlugin
+  Description = "Bot memory usage."
+  Commands = {
+    :memory => "returns bot memory usage"
+  }
+
+  def on_privmsg(msg)
+    case msg.botcommand
+      when :memory
+        pid, size = `ps ax -o pid,rss | grep -E "^[[:space:]]*#{$$}"`.strip.split.map(&:to_i)
+        msg.reply "Bot currently uses #{'%.2f' % (size / 1024)} MiB of memory!"
+    end
+  end
+end
