f468766750231c632ed0e075eb9e9ba1ecfa4a54
Thuruk <d.staudigl@gmx.at>
2013-09-12 15:31:14 +0200

    Use 'free' instead of 'ps' and show swap in Memory

--- IRC/plugins/Memory/Memory.rb
+++ IRC/plugins/Memory/Memory.rb
@@ -1,16 +1,20 @@
 require_relative '../../IRCPlugin'
 
 class Memory < IRCPlugin
-  Description = "Bot memory usage."
+  Description = "Monitors memory useage."
   Commands = {
-    :memory => "returns bot memory usage"
+    :free => "returns free memory"
   }
 
   def on_privmsg(msg)
-    case msg.bot_command
-      when :memory
-        pid, size = `ps ax -o pid,rss | grep -E "^[[:space:]]*#{$$}"`.strip.split.map(&:to_i)
-        msg.reply "Bot currently uses #{'%.2f' % (size / 1024)} MiB of memory!"
+    case msg.botcommand
+      when :free
+        free_array = `free`.split(" ")
+        mfree = free_array[8].to_i
+        mtotal = free_array[7].to_i
+        sfree = free_array[19].to_i
+        stotal = free_array[18].to_i
+        msg.reply "Memory usage: #{'%.2f' % (mfree / 1024)} of #{'%.2f' % (mtotal / 1024)} MiB \/\/ SWAP usage: #{'%.2f' % (sfree / 1024)} of #{'%.2f' % (stotal / 1024)} MiB"
     end
   end
 end
