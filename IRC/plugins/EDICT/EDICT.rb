cc16d75a7737f24365dce8bf1127d6c7c164b2c0
Alexander Belykh <albel727@ngs.ru>
2013-12-26 18:25:51 +0700

    Migrate EDICT to Sequel+Sqlite

--- IRC/plugins/EDICT/EDICT.rb
+++ IRC/plugins/EDICT/EDICT.rb
@@ -9,10 +9,17 @@
 #
 # http://www.csse.monash.edu.au/~jwb/edict.html
 
+require 'rubygems'
+require 'bundler/setup'
+require 'sequel'
+
 require_relative '../../IRCPlugin'
+require_relative '../../SequelHelpers'
 require_relative 'EDICTEntry'
 
 class EDICT < IRCPlugin
+  include SequelHelpers
+
   Description = 'An EDICT plugin.'
   Commands = {
     :j => 'looks up a Japanese word in EDICT',
@@ -28,13 +35,18 @@ See '.faq regexp'",
     @l = @plugin_manager.plugins[:Language]
     @m = @plugin_manager.plugins[:Menu]
 
-    @hash_edict = load_dict("edict")
+    @db = database_connect("sqlite://#{(File.dirname __FILE__)}/edict.sqlite", :encoding => 'utf8')
+
+    @hash_edict = load_dict(@db)
   end
 
   def beforeUnload
     @m.evict_plugin_menus!(self.name)
 
     @hash_edict = nil
+
+    database_disconnect(@db)
+    @db = nil
 
     @m = nil
     @l = nil
@@ -50,12 +62,12 @@ See '.faq regexp'",
       word = msg.tail
       return unless word
       l_kana = @l.kana(word)
-      edict_lookup = lookup(l_kana, [@hash_edict[:japanese], @hash_edict[:readings]])
+      edict_lookup = lookup([l_kana], [:japanese, :reading_norm])
       reply_with_menu(msg, generate_menu(format_description_unambiguous(edict_lookup), "\"#{word}\" #{"(\"#{l_kana}\") " unless word.eql?(l_kana)}in EDICT"))
     when :e
       word = msg.tail
       return unless word
-      edict_lookup = keyword_lookup(split_into_keywords(word), @hash_edict[:keywords])
+      edict_lookup = keyword_lookup(split_into_keywords(word))
       reply_with_menu(msg, generate_menu(format_description_show_all(edict_lookup), "\"#{word}\" in EDICT"))
     when :jr
       word = msg.tail
@@ -119,15 +131,20 @@ See '.faq regexp'",
     )
   end
 
-  # Looks up a word in specified hash(es) and returns the result as an array of entries
-  def lookup(word, hashes)
-    lookup_result = []
-    hashes.each do |h|
-      entry_array = h[word]
-      lookup_result |= entry_array if entry_array
+  # Looks up all entries that have each given word in any
+  # of the specified columns and returns the result as an array of entries
+  def lookup(words, columns)
+    table = @hash_edict[:edict_entries]
+
+    condition = Sequel.&(*words.map do |word|
+      Sequel.or(columns.map { |column| [column, word] })
+    end)
+
+    dataset = table.where(condition).group_by(Sequel.qualify(:edict_entry, :id))
+
+    standard_order(dataset).select(:japanese, :reading, :simple_entry, :id).to_a.map do |entry|
+      EDICTLazyEntry.new(table, entry[:id], entry)
     end
-    sort_result(lookup_result)
-    lookup_result
   end
 
   def lookup_complex_regexp(complex_regexp)
@@ -158,44 +175,85 @@ See '.faq regexp'",
     lookup_result
   end
 
-  # Looks up keywords in the keyword hash.
-  # Specified argument is a string of one or more keywords.
-  # Returns the intersection of the results for each keyword.
-  def keyword_lookup(words, hash)
-    lookup_result = nil
+  # Looks up all entries that contain all given words in english text
+  def keyword_lookup(words)
+    column = :text
 
-    words.each do |k|
-      return [] unless (entry_array = hash[k])
-      if lookup_result
-        lookup_result &= entry_array
-      else
-        lookup_result = Array.new(entry_array)
-      end
+    words = words.uniq
+
+    table = @hash_edict[:edict_entries]
+    edict_english = @hash_edict[:edict_english]
+    edict_english_join = @hash_edict[:edict_english_join]
+
+    condition = Sequel.|(*words.map do |word|
+      { Sequel.qualify(:edict_english, column) => word.to_s }
+    end)
+
+    english_ids = edict_english.where(condition).select(:id).to_a.map {|h| h.values}.flatten
+
+    return [] unless english_ids.size == words.size
+
+    dataset = edict_english_join.where(Sequel.qualify(:edict_entry_to_english, :edict_english_id) => english_ids).group_and_count(Sequel.qualify(:edict_entry_to_english, :edict_entry_id)).join(:edict_entry, :id => :edict_entry_id).having(:count => english_ids.size)
+
+    dataset = dataset.select_append(
+        Sequel.qualify(:edict_entry, :japanese),
+        Sequel.qualify(:edict_entry, :reading),
+        Sequel.qualify(:edict_entry, :simple_entry),
+        Sequel.qualify(:edict_entry, :id),
+    )
+
+    standard_order(dataset).to_a.map do |entry|
+      EDICTLazyEntry.new(table, entry[:id], entry)
     end
-    return [] unless lookup_result && !lookup_result.empty?
-    sort_result(lookup_result)
-    lookup_result
+  end
+
+  def standard_order(dataset)
+    dataset.order_by(Sequel.qualify(:edict_entry, :id))
   end
 
   def split_into_keywords(word)
     EDICTEntry.split_into_keywords(word).uniq
   end
 
-  def sort_result(lr)
-    lr.sort_by!{|e| e.sortKey} if lr
+  def load_dict(db)
+    edict_version = db[:edict_version]
+    edict_entries = db[:edict_entry]
+    edict_english = db[:edict_english]
+    edict_english_join = db[:edict_entry_to_english]
+
+    versions = edict_version.to_a.map {|x| x[:id]}
+    unless versions.include?(EDICTEntry::VERSION)
+      raise "The database version #{versions.inspect} of #{db.uri} doesn't correspond to this version #{[EDICTEntry::VERSION].inspect} of plugin. Rerun convert.rb."
+    end
+
+    regexpable = edict_entries.select(:japanese, :reading, :simple_entry, :id).to_a
+    regexpable = regexpable.map do |entry|
+      EDICTLazyEntry.new(edict_entries, entry[:id], entry)
+    end
+
+    {
+        :edict_entries => edict_entries,
+        :edict_english => edict_english,
+        :edict_english_join => edict_english_join,
+        :all => regexpable,
+    }
   end
 
-  def load_dict(dict_name)
-    dict = File.open("#{(File.dirname __FILE__)}/#{dict_name}.marshal", 'r') do |io|
-      Marshal.load(io)
-    end
-    raise "The #{dict_name}.marshal file is outdated. Rerun convert.rb." unless dict[:version] == EDICTEntry::VERSION
+  class EDICTLazyEntry
+    attr_reader :japanese, :reading, :simple_entry
+    lazy_dataset_field :raw, Sequel.qualify(:edict_entry, :id)
 
-    # Pre-parse all entries
-    dict[:all].each do |entry|
-      entry.parse
+    def initialize(dataset, id, pre_init)
+      @dataset = dataset
+      @id = id
+
+      @japanese = pre_init[:japanese]
+      @reading = pre_init[:reading]
+      @simple_entry = pre_init[:simple_entry]
     end
 
-    dict
+    def to_s
+      self.raw
+    end
   end
 end
