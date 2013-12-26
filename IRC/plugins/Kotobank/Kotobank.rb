4a7975d665d4deb9cb29948774aee6a7ae798ad4
Thuruk <d.staudigl@gmx.at>
2013-12-26 18:40:32 +0700

    Add Kotobank plugin

---
+++ IRC/plugins/Kotobank/Kotobank.rb
@@ -0,0 +1,21 @@
+require_relative '../../IRCPlugin'
+
+class Kotobank < IRCPlugin
+  Description = "Returns link to kotobank.jp/weblio.jp/kobun.weblio.jp lookup."
+  Commands = {
+    :du => "returns kotobank link",
+    :wl => "returns weblio link",
+    :wk => "returns kobun.weblio link"
+  }
+
+  def on_privmsg(msg)
+    case msg.botcommand
+      when :du
+        msg.reply Addressable::URI.encode( "http://kotobank.jp/word/#{msg.tail}?dic=daijirin" )
+      when :wl
+        msg.reply Addressable::URI.encode( "http://www.weblio.jp/content/#{msg.tail}" )
+      when :wk
+        msg.reply Addressable::URI.encode( "http://kobun.weblio.jp/content/#{msg.tail}" )
+    end
+  end
+end
