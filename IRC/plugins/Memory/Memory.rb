b40809d823537407349a9529993903708619cc02
Alexander Belykh <albel727@ngs.ru>
2013-12-28 04:27:20 +0700

    Read /proc/meminfo directly in Memory

--- IRC/plugins/Memory/Memory.rb
+++ IRC/plugins/Memory/Memory.rb
@@ -1,20 +1,29 @@
 require_relative '../../IRCPlugin'
 
 class Memory < IRCPlugin
-  Description = "Monitors memory useage."
+  Description = 'Monitors memory usage.'
   Commands = {
-    :free => "returns free memory"
+    :free => 'reports memory info'
   }
 
   def on_privmsg(msg)
     case msg.botcommand
       when :free
-        free_array = `free`.split(" ")
-        mfree = free_array[8].to_i
-        mtotal = free_array[7].to_i
-        sfree = free_array[19].to_i
-        stotal = free_array[18].to_i
-        msg.reply "Memory usage: #{'%.2f' % (mfree / 1024)} of #{'%.2f' % (mtotal / 1024)} MiB \/\/ SWAP usage: #{'%.2f' % (sfree / 1024)} of #{'%.2f' % (stotal / 1024)} MiB"
+        d = File.open('/proc/meminfo') do |f|
+          Hash[f.each_line.map do |l|
+            key, value = l.split()
+            [key.downcase.delete(':').to_sym, value.to_i]
+          end]
+        end
+
+        # Thanks to CalimeroTeknik for this memory calculation formula
+        mused = d[:memtotal] - d[:memfree] - d[:cached] - d[:buffers] + d[:shmem] + d[:slab] + d[:kernelstack] + d[:pagetables]
+        mtotal = d[:memtotal]
+
+        stotal = d[:swaptotal]
+        sused = stotal - d[:swapfree]
+
+        msg.reply "Memory usage: #{'%.2f' % (mused / 1024)} of #{'%.2f' % (mtotal / 1024)} MiB \/\/ SWAP usage: #{'%.2f' % (sused / 1024)} of #{'%.2f' % (stotal / 1024)} MiB"
     end
   end
 end
