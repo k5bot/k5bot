5d5dd616ae343f1c64842c7065f3d76903d6a79f
Alexander Belykh <albel727@ngs.ru>
2014-08-07 17:44:22 +0700

    Remove hack for old ting in Pinyin

--- IRC/plugins/Pinyin/Pinyin.rb
+++ IRC/plugins/Pinyin/Pinyin.rb
@@ -11,10 +11,6 @@ require 'bundler/setup'
 require 'ting'
 require 'ruby-pinyin'
 
-# Hack: remove Pinyin = Ting compatibility alias, defined in ting,
-# because it conflicts with our class name.
-Object.send(:remove_const, 'Pinyin')
-
 class Pinyin < IRCPlugin
   Description = "Hanzi conversion plugin."
   Commands = {
