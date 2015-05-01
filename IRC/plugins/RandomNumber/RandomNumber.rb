df6265294469b7c8c0e9e989e578f54d67e7dc1c
Johan Sageryd <j@1616.se>
2015-05-01 19:14:24 +0200

    Add RandomNumber plugin

---
+++ IRC/plugins/RandomNumber/RandomNumber.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+# This file is part of the K5 bot project.
+# See files README.md and COPYING for copyright and licensing information.
+
+require_relative '../../IRCPlugin'
+
+class RandomNumber < IRCPlugin
+  Description = "RandomNumber plugin."
+  Commands = {
+    :randomnumber => "gives a random number",
+  }
+
+  def on_privmsg(msg)
+    case msg.botcommand
+    when :randomnumber
+      msg.reply(random_number)
+    end
+  end
+
+  def random_number
+    4
+  end
+end
