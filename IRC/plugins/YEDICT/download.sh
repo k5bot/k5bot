6997a7dc92ef0bb4012712853b8cef5bdebe155a
Tenki <saamcyun@gmail.com>
2015-11-08 04:36:59 -0500

    change http to https in yedict downloadlink

--- IRC/plugins/YEDICT/download.sh
+++ IRC/plugins/YEDICT/download.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # This file is part of the K5 bot project.
 # See files README.md and COPYING for copyright and licensing information.
-curl http://dl.dropboxusercontent.com/u/32882273/yedict_20100108.u8 > yedict
+curl https://dl.dropboxusercontent.com/u/32882273/yedict_20100108.u8 > yedict
