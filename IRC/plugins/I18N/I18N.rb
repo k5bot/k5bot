1a2f4fc7d9f290dfe13051d77f9cedf5b7decb4e
Alexander Belykh <albel727@ngs.ru>
2016-02-06 17:38:51 +0600

    Hide I18N commands behind can_do_everything perm

--- IRC/plugins/I18N/I18N.rb
+++ IRC/plugins/I18N/I18N.rb
@@ -16,26 +16,47 @@ class I18N < IRCPlugin
       :i18n_reload => 'reloads i18n translation files',
       :i18n_set => 'set i18n locale',
   }
+  Dependencies = [ :Router ]
 
   def on_privmsg(msg)
-    case msg.bot_command
-      when :i18n_reload
-        I18n.load_path = Dir[File.join(File.dirname(__FILE__), 'locales', '*.yml')]
-        I18n.backend.load_translations
-        msg.reply("Reloaded translations. Available locales: #{format_available_locales}")
-      when :i18n_set
-        new_locale = msg.tail && msg.tail.to_sym
-        unless I18n::available_locales.include?(new_locale)
-          msg.reply("Unknown locale. Available locales: #{format_available_locales}")
-          return
-        end
-
-        msg.reply("Changed I18n locale to #{new_locale}. Previous locale: #{I18n.locale || I18n.default_locale}")
-        I18n.locale = I18n.default_locale = new_locale
+    dispatch_message_by_command(msg, Commands.keys) do
+      check_and_complain(@plugin_manager.plugins[:Router], msg, :can_do_everything)
     end
+  end
+
+  def cmd_i18n_reload(msg)
+    I18n.load_path = Dir[File.join(File.dirname(__FILE__), 'locales', '*.yml')]
+    I18n.backend.load_translations
+    msg.reply("Reloaded translations. Available locales: #{format_available_locales}")
+  end
+
+  def cmd_i18n_set(msg)
+    new_locale = msg.tail && msg.tail.to_sym
+    unless I18n::available_locales.include?(new_locale)
+      msg.reply("Unknown locale. Available locales: #{format_available_locales}")
+      return
+    end
+
+    msg.reply("Changed I18n locale to #{new_locale}. Previous locale: #{I18n.locale || I18n.default_locale}")
+    I18n.locale = I18n.default_locale = new_locale
   end
 
   def format_available_locales
     I18n::available_locales.map(&:to_s).join(', ')
+  end
+
+  private
+
+  def check_and_complain(checker, msg, permission)
+    if checker.check_permission(permission, msg_to_principal(msg))
+      true
+    else
+      msg.reply("Sorry, you don't have '#{permission}' permission.")
+      false
+    end
+  end
+
+  def msg_to_principal(msg)
+    msg.principals.first
   end
 end
