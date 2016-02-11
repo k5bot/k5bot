3376350269915114f021ce03b4b3f28e8ef65bab
Alexander Belykh <albel727@ngs.ru>
2016-02-11 16:28:07 +0600

    Implement simple dice notation support in Dice

--- IRC/plugins/Dice/Dice.rb
+++ IRC/plugins/Dice/Dice.rb
@@ -9,28 +9,53 @@ require_relative '../../IRCPlugin'
 class Dice < IRCPlugin
   Description = 'Dice plugin.'
   Commands = {
-    :roll => 'rolls the dice',
+    :roll => "rolls the dice. Supports simple dice notation \
+(e.g. 2d6 means roll two 6-sided dice, the default when the argument is omitted)",
   }
 
   def on_privmsg(msg)
     case msg.bot_command
     when :roll
-      die1 = roll
-      die2 = roll
-      roll_result = '%s %s ⇒ %s' % [ number_to_die(die1), number_to_die(die2), number_to_text(die1 + die2) ]
-      msg.reply(roll_result)
+      spec = msg.tail || '2d6'
+      m = spec.match(/^(\d{1,6})[dD](\d{1,6})/)
+      if m
+        to_roll = m[1].to_i
+        num_faces = m[2].to_i
+        rolls = to_roll.times.map do
+          rand(num_faces) + 1
+        end
+
+        result = rolls.inject(0, &:+)
+        prefix = if rolls.size < 10
+           rolls.map {|r| number_to_die(r, num_faces)}.join(' ')
+        else
+          'Avg: %.4g' % [result.to_f / rolls.size]
+        end
+
+        #roll_result = '%s %s ⇒ %s' % [ number_to_die(die1), number_to_die(die2), number_to_text(die1 + die2) ]
+        msg.reply("#{prefix} ⇒ #{result}")
+      else
+        msg.reply("Unknown dice notation: #{spec}")
+      end
     end
   end
 
-  def roll
-    rand(6) + 1
+  def number_to_die(num, num_faces)
+    if num_faces <= 6
+      "|#{'•' * num}|"
+    else
+      "|#{num}|"
+    end
   end
 
-  def number_to_die(num)
-    '|%s|' % ['•' * num]
-  end
+  #TODO: reimplement with I18N
+  NUMERALS = %w(zero one two three four five six seven eight nine ten eleven twelve)
 
   def number_to_text(num)
-    %w(one two three four five six seven eight nine ten eleven twelve)[num - 1]
+    if num < NUMERALS.size
+      NUMERALS[num]
+    else
+      num.to_s
+    end
   end
 end
