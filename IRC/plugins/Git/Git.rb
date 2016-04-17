c0e7ac9e1152edd408ba9c3fd93e490b52ca0303
Alexander Belykh <albel727@ngs.ru>
2016-04-18 03:52:04 +0600

    Improve style in Git plugin a bit

--- IRC/plugins/Git/Git.rb
+++ IRC/plugins/Git/Git.rb
@@ -27,28 +27,28 @@ class Git < IRCPlugin
   def on_privmsg(msg)
     case msg.bot_command
     when :pull
-      versionBefore = @statistics.version_string
-      gitPull
-      versionAfter = @statistics.version_string
-      if versionBefore == versionAfter
+      version_before = @statistics.version_string
+      git_pull
+      version_after = @statistics.version_string
+      if version_before == version_after
         msg.reply('Already up-to-date.')
       else
-        msg.reply("Updating #{versionBefore} -> #{versionAfter}")
+        msg.reply("Updating #{version_before} -> #{version_after}")
       end
     when :upstream
-      msg.reply(gitUpstream)
+      msg.reply(git_upstream)
     end
   end
 
-  def gitPull
+  def git_pull
     `pushd #{File.dirname($0)} && $(which git) fetch && $(which git) reset --hard @{upstream} && popd`
   end
 
-  def gitUpstream
+  def git_upstream
     upstream = `pushd #{File.dirname($0)} >/dev/null 2>&1 && $(which git) branch -vv | grep -e '^\*' && popd >/dev/null 2>&1`[/\[([^\[]+)\]/, 1]
     remote = upstream[/([^\/]+)\/([^:]+).*/, 1]
     branch = upstream[/([^\/]+)\/([^:]+).*/, 2]
-    remoteURL = `pushd #{File.dirname($0)} >/dev/null 2>&1 && $(which git) remote -v | grep #{remote} | head -1 && popd >/dev/null 2>&1`.split[1]
-    "upstream is [#{branch}] at #{remoteURL}"
+    remote_url = `pushd #{File.dirname($0)} >/dev/null 2>&1 && $(which git) remote -v | grep #{remote} | head -1 && popd >/dev/null 2>&1`.split[1]
+    "upstream is [#{branch}] at #{remote_url}"
   end
 end
