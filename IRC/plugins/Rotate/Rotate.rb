b7f7a8cde3fcd2a8ee502820ddb2bbd42194ac9c
Alexander Belykh <albel727@ngs.ru>
2016-04-18 18:59:54 +0600

    Add .rot13 to Rotate

--- IRC/plugins/Rotate/Rotate.rb
+++ IRC/plugins/Rotate/Rotate.rb
@@ -12,30 +12,44 @@ class Rotate < IRCPlugin
   }
 
   def on_privmsg(msg)
+    return unless msg.tail
     case msg.bot_command
+      when :rot13
+        text = msg.tail
+        text = do_rotate(text, ROT13_COMMANDS)
+        msg.reply(text)
       when :rotate
-        rotate(msg)
+        number, *text = msg.tail.split(' ')
+        number = number.to_i
+        text = text.join
+        return unless number != 0 && text.length > 0
+
+        text = do_rotate(text, [[number, UNICODE_BMP_LOWER, UNICODE_BMP_UPPER]])
+        msg.reply(text)
     end
   end
 
-  def rotate(msg)
-    return unless msg.tail
+  def do_rotate(text, commands)
+    text = text.unpack('U*')
 
-    text = msg.tail.split(' ')
-    number = text.shift
-    number = number.to_i
-    text = text.join
+    commands.each do |number, lower_bound, upper_bound|
+      upper_bound += 1
+      text.map! do |c|
+        next c unless c >= lower_bound && c < upper_bound
+        lower_bound + (c + number - lower_bound) % (upper_bound - lower_bound)
+      end
+    end
 
-    return unless number != 0 && text.length > 0
-
-    rotated = text.unpack('U*').map do |c|
-      next unless c >= LOWER_BOUND && c < UPPER_BOUND
-      LOWER_BOUND + (c + number - LOWER_BOUND) % (UPPER_BOUND - LOWER_BOUND)
-    end.compact.pack('U*')
-
-    msg.reply(rotated)
+    text.pack('U*')
   end
 
-  UPPER_BOUND = 0xE000
-  LOWER_BOUND = 0
+  private
+
+  ROT13_COMMANDS = [
+      [13, *'az'.unpack('U*')],
+      [13, *'AZ'.unpack('U*')],
+  ]
+
+  UNICODE_BMP_LOWER = 0
+  UNICODE_BMP_UPPER = 0xDFFF
 end
