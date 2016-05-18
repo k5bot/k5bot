e5f6bf5e93582787402b62e42502bc2246ff8499
Alexander Belykh <albel727@ngs.ru>
2016-05-19 03:12:29 +0600

    Add lang-restricted commands to Googler

--- IRC/plugins/Googler/Googler.rb
+++ IRC/plugins/Googler/Googler.rb
@@ -14,10 +14,22 @@ class Googler
   include IRCPlugin
   DESCRIPTION = 'Provides access to various Google services'
   COMMANDS = {
-      :g => 'searches Google and returns the first result',
-      :g? => 'searches Google and returns results as a menu',
-      :'g#' => 'searches Google and returns estimated hit count',
+      :g => 'searches Google and returns the first result (.gja for japanese restricted search, etc).',
+      :g? => 'searches Google and returns results as a menu (.gja? for japanese restricted search, etc).',
+      :'g#' => 'searches Google and returns estimated hit count (.gja# for japanese restricted search, etc).',
   }
+
+  def self.make_lang_service_format_map(arr, h)
+    arr.zip(arr).to_h.merge(h)
+  end
+
+  def self.make_command_regex(h)
+    Regexp.new('^g(' + Regexp.union(h.keys.sort_by(&:length).reverse).source + ')?([\?#]?)$')
+  end
+
+  # noinspection RubyStringKeysInHashInspection
+  GOOGLE_SUPPORTED = make_lang_service_format_map(%w(en ja ko fr pt de it es no ru fi hu sv da pl lt nl ar sr el), {'zh' => 'zh-CN', 'tw' => 'zh-TW'})
+  COMMAND_MATCHER = make_command_regex(GOOGLE_SUPPORTED)
 
   DEPENDENCIES = [:Menu]
 
@@ -34,18 +46,23 @@ class Googler
   end
 
   def on_privmsg(msg)
-    case msg.bot_command
-      when :g?
+    m = msg.bot_command.to_s.match(COMMAND_MATCHER)
+    return unless m
+    lang = GOOGLE_SUPPORTED[m[1]]
+    cmd_type = m[2]
+
+    case cmd_type
+      when '?'
         word = msg.tail
         return unless word
-        lookup = find_item(word, 10).map do |item|
+        lookup = find_item(word, 10, lang).map do |item|
           [item['title'], item['link'], item['snippet']]
         end
         reply_with_menu(msg, generate_menu(lookup, "\"#{word}\" in Google"))
-      when :g
+      when ''
         word = msg.tail
         return unless word
-        lookup = find_item(word, 1).map do |item|
+        lookup = find_item(word, 1, lang).map do |item|
           [item['title'], item['link'], item['snippet']]
         end
         if lookup.empty?
@@ -56,10 +73,10 @@ class Googler
             msg.reply("#{snippet}") if snippet
           end
         end
-      when :'g#'
+      when '#'
         word = msg.tail
         return unless word
-        lookup = find_count(word)
+        lookup = find_count(word, lang)
         msg.reply("Estimated #{lookup} hit(s) for \"#{word}\" in Google")
     end
   end
@@ -80,19 +97,26 @@ class Googler
     )
   end
 
-  def find_item(query, size)
-    search = perform_query(query, size, 'items(title,link,snippet)')
+  def find_item(query, size, lang = nil)
+    search = perform_query(query, size, lang, 'items(title,link,snippet)')
     search['items'].take(size)
   end
 
-  def find_count(query)
-    search = perform_query(query, 1, 'searchInformation/formattedTotalResults')
+  def find_count(query, lang = nil)
+    search = perform_query(query, 1, lang, 'searchInformation/formattedTotalResults')
     search['searchInformation']['formattedTotalResults']
   end
 
-  def perform_query(query, size, fields = nil)
+  def perform_query(query, size, lang = nil, fields = nil)
     options = @config.dup.merge(:num => [10, size].min, :ie => 'utf8', :oe => 'utf8')
     options[:fields] = fields if fields
+    if lang && !lang.empty?
+      options[:hl] = lang
+      options[:lr] = "lang_#{lang}"
+    else
+      options.delete(:hl)
+      options.delete(:lr)
+    end
     GoogleCustomSearchApi.search(query, options)
   end
 end
