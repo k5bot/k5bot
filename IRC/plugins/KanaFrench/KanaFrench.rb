7e46334493daed4c4f5a77a12b28801cea73eddd
Alexander Belykh <albel727@ngs.ru>
2016-05-19 04:11:44 +0600

    Rename French plugin to KanaFrench

--- IRC/plugins/KanaFrench/KanaFrench.rb
+++ IRC/plugins/KanaFrench/KanaFrench.rb
@@ -2,7 +2,7 @@
 # This file is part of the K5 bot project.
 # See files README.md and COPYING for copyright and licensing information.
 
-# French plugin
+# KanaFrench plugin
 
 require 'yaml'
 require_relative '../../IRCPlugin'
