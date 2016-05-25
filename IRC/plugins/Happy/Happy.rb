99f7e4a0a87044203d22d33d2a931db2e5056d56
Alexander Belykh <albel727@ngs.ru>
2016-05-25 23:04:21 +0600

    Extract smiles to txts in Happy, implement reply categories

--- IRC/plugins/Happy/Happy.rb
+++ IRC/plugins/Happy/Happy.rb
@@ -10,49 +10,55 @@ class Happy
   include IRCPlugin
   DESCRIPTION = ':D'
 
-  PATTERNS = [
-    ':)',
-    ':D',
-    'D:',
-    '^^',
-    ':|',
-    ':/',
-    ':O',
-    ':#',
-    ':v',
-    '\(^o^)/',
-    'o_O',
-    'O_O',
-    'O_o',
-    'O_( )',
-    '囧',
-    '冏',
-    ':C',
-    '(:',
-    ':x',
-    'XD',
-    '8|',
-    ':@',
-    '-_-',
-    ':(',
-    '):',
-    ';)',
-    '=)',
-    '=D',
-    ':P',
-    '::)',
-    ':))',
-    'o/',
-    '\o'
-  ]
+  PATTERN_ROUTING = {
+      :table_flip => [:table_set],
+      :table_set => [:table_flip],
+  }
 
-  PATTERN_REGEXP = /^\s*(#{PATTERNS.map { |s| Regexp.quote(s) }.join('|')})+\s*$/
+  def afterLoad
+    h = {}
+
+    load_txt!(:smile, h)
+    load_txt!(:sadness, h)
+    load_txt!(:surprize, h)
+    load_txt!(:table_flip, h)
+    load_txt!(:table_set, h)
+
+    @patterns = h
+    @pattern_regexp = /^\s*(?:(#{Regexp.union(h.values.flatten(1)).source})\s*)+$/
+  end
+
+  def beforeUnload
+    @pattern_regexp = nil
+    @patterns = nil
+
+    nil
+  end
 
   def on_privmsg(msg)
     tail = msg.tail
     return unless tail && !msg.bot_command
-    if tail =~ PATTERN_REGEXP
-      msg.reply(PATTERNS.sample)
+
+    m = @pattern_regexp.match(tail)
+    return unless m
+
+    category, _ = @patterns.find do |_, values|
+      values.include?(m[1])
     end
+
+    routes = PATTERN_ROUTING[category] || [category]
+
+    msg.reply(routes.flat_map {|r| @patterns[r]}.sample)
+  end
+
+  private
+
+  def load_txt!(name, hash)
+    hash[name] = File.read(File.join(self.plugin_root, "#{name}.txt")).each_line.map do |l|
+      l.chomp!
+      l.strip!
+      next if l.empty?
+      l
+    end.compact.sort_by(&:size).reverse
   end
 end
