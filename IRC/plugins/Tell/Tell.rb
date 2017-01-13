02deebf34d8ced5ad0c479488678d625907c1867
Thuruk <d.staudigl@gmx.at>
2017-01-13 19:24:59 +0100

    More helpful message in spam filter

--- IRC/plugins/Tell/Tell.rb
+++ IRC/plugins/Tell/Tell.rb
@@ -92,7 +92,7 @@ class Tell
       if tell_messages.index { |_, _, m| m == tell_message }
         msg.reply("#{msg.nick}: Already noted.")
       elsif tell_messages.length >= 5
-        msg.reply("#{msg.nick}: How about you do that when #{user.nick} returns. Yeah that would be great.")
+        msg.reply("#{msg.nick}: Please do that when #{user.nick} returns or use the memo command.")
       else
         tell_messages << [Time.now, msg.nick, tell_message]
         store
