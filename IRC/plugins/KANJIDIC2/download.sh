ab0e67948aac8e27a9f4b1a6714e00f837058ed7
CalimeroTeknik <calimeroteknik@free.fr>
2023-09-17 18:52:06 +0200

    Plugin KANJIDIC2: update download.sh

--- IRC/plugins/KANJIDIC2/download.sh
+++ IRC/plugins/KANJIDIC2/download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # This file is part of the K5 bot project.
 # See files README.md and COPYING for copyright and licensing information.
-curl http://ftp.monash.edu.au/pub/nihongo/kanjidic2.xml.gz | gunzip > kanjidic2.xml
-curl http://ftp.monash.edu.au/pub/nihongo/kradfile-u.gz | gunzip > kradfile-u.txt
+curl http://www.edrdg.org/kanjidic/kanjidic2.xml.gz | gunzip > kanjidic2.xml
+curl http://ftp.edrdg.org/pub/Nihongo/kradfile.gz | gunzip | iconv -f euc-jp -t utf-8 > kradfile-u.txt
 curl http://ck.kolivas.org/Japanese/sorted_freq_list.txt > gsf.txt
