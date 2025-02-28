ac8f2cc0b6b87cdc39005660db2a66974c2435c6
CalimeroTeknik <calimeroteknik@free.fr>
2025-02-28 22:49:02 +0100

    Add a script for ruby to see all bundler gems.
    The reasons why this is needed to run the bot are not known yet:
    rubyeb19 requires this to be seen by ruby, but not all git gems do.
    However, there is no issue loading rubyeb19 from `bundle exec ruby`,
    or when using `require 'bundler/setup'`, which is an equivalent,
    hence further investigation is required.
    
    Typical use case: ./bundlexec.sh ./bot.rb

---
+++ bundlexec.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# This solves the baroque exception that (some?) gems installed from git cannot be loaded from bare ruby with `require`.
+
+IFS=:
+for path in $(gem env GEM_PATH);do
+	for lib in "$path"/bundler/gems/*/lib;do
+		if [ -z "$rubylib" ];then
+			rubylib="$lib"
+		else
+			rubylib="$rubylib:$lib"
+		fi
+	done
+done
+
+RUBYLIB="$rubylib" "$@"
